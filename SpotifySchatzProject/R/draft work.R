c6f8b729fef7ac2a35c9aa88e2f7e03a2eb95a61
Elizabeth Schatz <Eschatz@usf.edu>
2021-04-01 14:47:20 -0400

    Start repository
    woo a repository

---
+++ SpotifySchatzProject/R/draft work.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+install.packages("spotifyr")
+library(spotifyr)
+
+
+get_my_recently_played <- function(limit = 50)
+my_function = function(x){get_my_recently_played(limit = 50)}
+
+#I want to chart the songs played most often in a chart of the top 50 songs reently played to see if there are songs the user is played multiple times.
+
+
+
+
+#' SpotifySchatzProject
+
+#'
+
+#' Pull 50 recently played songs and graph
+
+#'
+
+#' Pull the most 50 recently played songs from a user, then grpah those songs according to most played to least played
+
+#'
+
+#' @author Elizabeth Schatz
+
+#'
---
+++ SpotifySchatzProject/R/starting stuff.R
@@ -0,0 +1,3 @@
+library(tidyverse)
+install.packages("spotifyr")
+library(spotifyr)
