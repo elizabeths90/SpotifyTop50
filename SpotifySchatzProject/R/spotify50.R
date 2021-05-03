aa28b02bcf9bc4350c201e81fe4703f6f1b47b8b
Elizabeth Schatz <Eschatz@usf.edu>
2021-05-03 19:08:07 -0400

    change

--- SpotifySchatzProject/R/spotify50.R
+++ SpotifySchatzProject/R/spotify50.R
@@ -12,14 +12,15 @@
 #' 
 #' @export
 
+#get your personal id and secret from Spotify
 spotify50 = function(id, secret){
   
   Sys.setenv(SPOTIFY_CLIENT_ID = id)
   Sys.setenv(SPOTIFY_CLIENT_SECRET = secret)
   access_token = get_spotify_access_token()
- 
+#set limit to desired number, for this case it is 50 
   new_object = spotifyr::get_my_recently_played(limit = 50)
-  
+#plot using esquisser code 
   new_object %>% ggplot2::ggplot [ESQUISSSER CODE HERE] 
   
 }
